e5f21622584c2da9deed19107ad27c3692cd2127
subsist23 <dancrichton@hotmail.com>
2023-09-08 16:24:19 +1200

    commit test#2

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -2,36 +2,46 @@ const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
-  pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.scale_for_screen(false);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image_sequence("flower" , "png", "13")
+  pScope.load_image_sequence("flowerC" , "png", "13")
+pScope.load_image("gd play chrome" , "png")
+ 
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 230);  //lets us draw the whole circle background, ignoring the boundaries
 
   //var layer1 = new PLayer(faces);
   //layer1.mode( SWIRL(5) );
   //layer1.set_boundary( 200, 1000 );
 
+  var layer2 = new PLayer(chrome);
+ layer2.mode( RING );
+  layer2.set_boundary( 0, 100 );
+
+
   var layer1 = new PLayer(flowers);
   layer1.mode( SWIRL(1) );
-  layer1.set_boundary( 500, 510 );
-
+  layer1.set_boundary( 722.3, 723 );
 
  // var layer2 = new PLayer(squares);
-  //layer2.mode( RING );
-  //layer2.set_boundary( 0, 400 );
+//layer2.mode( RING );
+ //layer2.set_boundary( 0, 400 );
+
+ 
+
+
 }
 
 function flowers(x, y, animation, pScope){
   
   //scale(animation.frame*1);
-scale(2.3)
-  pScope.draw_image_from_sequence("flower", x, y, animation.frame);
+scale(3.3)
+  pScope.draw_image_from_sequence("flowerC", x, y, animation.frame);
 
 }
 
@@ -42,10 +52,27 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  noStroke()
+  fill(10,155,100)
+  arc(x,y,900,900,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+
+  noStroke()
+  fill(190,255,200)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  fill(0)
+  ellipse(0,-300-animation.wave()*50,10,10) // .wave is a cosine wave btw
+
+  fill(0)
+  ellipse(0,-200-animation.wave()*50,30,30) // .wave is a cosine wave btw
 
 }
+
+function chrome(x, y, animation, pScope){
+
+  scale(4)
+  pScope.draw_image("gd play chrome",0,0);
+push (2);
+pop (1);
+}
+
